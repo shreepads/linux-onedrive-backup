b33e77be7389fd8f18c5f042dd011bceb6729ef3
Shreepad S <shreepads@gmail.com>
2020-03-01 13:59:53 +0530

    Change to scope supported for MS Accounts

--- webapp/app_config.py
+++ webapp/app_config.py
@@ -29,7 +29,7 @@ ENDPOINT = 'https://graph.microsoft.com/v1.0/users'  # This resource requires no
 
 # You can find the proper permission names from this document
 # https://docs.microsoft.com/en-us/graph/permissions-reference
-SCOPE = ["User.ReadBasic.All"]
+SCOPE = ["User.Read"]
 
 
 SESSION_TYPE = "filesystem"  # So token cache will be stored in server-side session
